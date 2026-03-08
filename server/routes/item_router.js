32867127c46975b152fb274efb5fb271b906e641
Your Name <your-email@example.com>
2026-03-08 10:44:00 -0700

    cleaning up

--- server/routes/item_router.js
+++ server/routes/item_router.js
@@ -1,37 +1,12 @@
-import {Router} from "express";
-import * as itemService from '../services/item_service.js';
+import { Router } from 'express';
 const router = Router();
 
 // GET ITEMS
-router.get('/:orderId', async(req, res) => {
-
-    const order_id = Number(req.params.orderId);
-    if(!order_id ){
-        return res.status(400).json({
-            status: 'fail', 
-            message: 'Invalid Request User ID format'
-        })
-    }
-    try {
-        const result = await itemService.getItermsFromOrder(order_id);
-        return res.status(200).json({ 
-            status: 'success', 
-            data: { result } 
-        });
-    } catch(err){
-        res.status(err.statusCode || 500).json({ 
-        status: 'fail',
-        message: err.message || 'Internal Server Error' })
-    };
-})
 
 // POST ITEMS
 
-
 // PUT ITEMS
 
-
 // DELTE ITEMS
-
 
 export default router;
