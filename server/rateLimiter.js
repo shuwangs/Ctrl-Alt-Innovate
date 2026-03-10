ad27a1da182ce575b9cccb023aa9f30474994480
shu.wang <shuw425@gmail.com>
2026-03-09 22:08:57 -0400

    update readme

--- server/rateLimiter.js
+++ server/rateLimiter.js
@@ -2,12 +2,12 @@ import {rateLimit} from 'express-rate-limit';
 
 const limiter = rateLimit({
     windowMs:15 * 60 * 1000,
-    max: 100,
+    max: 3,
     standardHeaders: 'draft-8',
     legacyHeaders: false,
     ipv6Subnet: 56,
     statusCode: 429,
-    message: 'You have exceeded the 3 requests in 24 hrs limit!', 
+    message: 'Too many request in 15mins!', 
 
 })
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 
 import app from './app.js';
 import dotenv from 'dotenv';
-import rateLimiter from './rateLimiter.js';
 dotenv.config();
 
 const port = process.env.PORT || 3001;
